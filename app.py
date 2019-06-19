d14472de11caa2866b7280ee0e86454b28b6fb57
aaronshivers <aaron@moonmail.org>
2019-06-19 16:29:05 -0500

    added gitignore, got db running

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 # Import Dependencies
 from flask import Flask, render_template, url_for
-from flask_alchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 
 # Initialize Application
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Database Setup
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize Database
 db = SQLAlchemy(app)
 
 # Todo Class/Model
-class = Todo(db.Model):
+class Todo(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   content = db.Column(db.String(200), nullable=False)
   date_created = db.Column(db.DateTime, default=datetime.utcnow)
