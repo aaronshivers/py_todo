b5e71283b5aae00fc078977e67a80f2d55a04bc2
aaronshivers <aaron@moonmail.org>
2019-06-19 16:22:32 -0500

    setup db and todo model

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 # Import Dependencies
 from flask import Flask, render_template, url_for
+from flask_alchemy import SQLAlchemy
+from datetime import datetime
 
 # Initialize Application
 app = Flask(__name__)
+
+# Database Setup
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+
+# Initialize Database
+db = SQLAlchemy(app)
+
+# Todo Class/Model
+class = Todo(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  content = db.Column(db.String(200), nullable=False)
+  date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
 
 # GET /
 @app.route('/')
